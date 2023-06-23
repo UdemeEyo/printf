c992e19a8ff57f19ee91b2d630b3654ce18570ba
UdemeEyo <udemeeyo2013@gmail.com>
2023-06-23 14:57:45 -0700

    _printf.c

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a group project by Udeme Eyo and Divine Bayuo which aims at helping us learn collaboration as programmers and to write a function similar to printf.
---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define BUFF_SIZE 1024
+#define UNUSED(x) (void)(x)
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+
+
+#endif
