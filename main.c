5975f127f68719457cbaa5cca868d63cfa4f764b
UdemeEyo <udemeeyo2013@gmail.com>
2023-06-24 07:12:58 -0700

    myPrintf.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * main - test codes
+ *
+ * Return: 0 on success
+ */
+int main(void)
+
+	_printf("%c\n", 'H');
+	_printf("%s\n", "House");
+	_printf("%d\n", 12);
+	_printf("%i\n", 2);
+	return (0);
+}
--- main.h
+++ main.h
@@ -4,5 +4,6 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
+int main(void);
 
 #endif
