b44f6d62f6ba6453b94afad7ae18747bdcb98856
Divine Bayuo <divinewb123@gmail.com>
2023-06-24 12:54:28 +0000

    Create myPrintf.c

---
+++ myPrintf.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf - function that produces output
+ * according to a format
+ * @format: pointer to the formats
+ * @...: other unspecified parameters
+ *
+ * Return: Always 0 (success)
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list argument;
+
+	va_start(argument, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+				printf("%c", va_arg(argument, int));
+
+			else if (*format == 's')
+				printf("%s", va_arg(argument, char *));
+
+			format++;
+		}
+		else
+		{
+			putchar(*format);
+		}
+		format++;
+	}
+	va_end(argument);
+}
